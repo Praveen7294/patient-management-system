79aff0da20d4532a2fd2cd5324f416cd88d90f82
Praveen Kumar <praveen729483@gmail.com>
2025-09-12 03:15:52 +0530

    Added logic in auth-service module to generate token using user email and password

---
+++ auth-service/src/main/java/org/example/authservice/controller/AuthController.java
@@ -0,0 +1,38 @@
+package org.example.authservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.example.authservice.dto.LoginRequestDTO;
+import org.example.authservice.dto.LoginResponseDTO;
+import org.example.authservice.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Generate tokens on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @RequestBody LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+
+        if (tokenOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
