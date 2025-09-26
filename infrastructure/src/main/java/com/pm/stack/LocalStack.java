e63f1366809afede80d17b94d8c8ca64b33d9910
Praveen Kumar <praveen729483@gmail.com>
2025-09-26 19:18:16 +0530

    Added a logic to create a Vpc

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,11 +1,27 @@
 package com.pm.stack;
 
-import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.AppProps;
+import software.amazon.awscdk.BootstraplessSynthesizer;
 
 public class LocalStack extends Stack {
 
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "PatientManagementVpc")
+                .vpcName("PatientManagementVpc")
+                .maxAzs(2)
+                .build();
     }
 
     public static void main(String[] args) {
